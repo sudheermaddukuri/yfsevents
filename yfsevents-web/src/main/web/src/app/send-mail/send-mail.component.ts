2a3f3bbb0603ff46bef5fc60e3818911a08b6e24
tusharchitti <rainatushar221995@gmail.com>
2019-02-27 17:15:56 +0530

    Send Mail Feature Added

---
+++ yfsevents-web/src/main/web/src/app/send-mail/send-mail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Email} from "../Email";
+import {FormBuilder} from "@angular/forms";
+import {ApiService} from "../api.service";
+import {ApiServiceMail} from "../api.service.mail";
+
+@Component({
+  selector: 'app-send-mail',
+  templateUrl: './send-mail.component.html',
+  styleUrls: ['./send-mail.component.css']
+})
+export class SendMailComponent implements OnInit {
+  private email:Email;
+  constructor(
+              private apiServiceMail: ApiServiceMail) { }
+  ngOnInit() {
+    this.email= new Email({to:"rainatushar221995@gmail.com,random1@gmail.com",cc:"",bcc:"",subject:"",text:""});
+  }
+  public onFormSubmit({value}:{value:Email}) {
+    console.log(value);
+    this.apiServiceMail.postData(this.email,"String");
+  }
+  // {to:"to",cc:"cc",bcc:"bcc",subject:"subject",text:"text"}
+}
