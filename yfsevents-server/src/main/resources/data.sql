2c19428158324d6433b15bf9d37e38396aad5401
tusharchitti <rainatushar221995@gmail.com>
2019-04-01 12:52:45 +0530

    Ui changes

--- yfsevents-server/src/main/resources/data.sql
+++ yfsevents-server/src/main/resources/data.sql
@@ -7,3 +7,5 @@ insert into authorized_person(id, name, partner_ngo_id) values (2, 'test person 
 
 insert into User (id, username, password, role) values (1, 'admin', '{noop}admin', 'ROLE_ADMIN');
 
+INSERT INTO VOLUNTEER(CITY,EMAIL,FIRST_NAME,LAST_NAME,LOCALITY,PHONE_NUMBER,PHONE_PREFIX,PINCODE,STATE)  VALUES ( 'jammu','rainatushar221995@gmail.com','Tushar','Raina','locality','8146222129','91','31274','j&k' );
+INSERT INTO VOLUNTEER(CITY,EMAIL,FIRST_NAME,LAST_NAME,LOCALITY,PHONE_NUMBER,PHONE_PREFIX,PINCODE,STATE)  VALUES ( 'hyd','ranom22@gmail.com','T','Raina','locality','3434344343','91','31274','Hyerabad' );
